ddb55fd67a2787c21df9f50b58c4358ba5084c0d
sheridan <you@example.com>
2022-09-06 13:04:42 -0500

    Header Update

--- customer_country.py
+++ customer_country.py
@@ -9,7 +9,29 @@ outfile = open('customer_country.csv' , 'w')
 
 next(csvfile)
 
+outfile.write('Full Name, Country\n')
+
 for row in csvfile:
     outfile.write(row[1] + ' ' + row[2] + ', ' + row[4] +'\n')
 
 outfile.close()
+
+#or
+
+#import csv
+
+#infile = open('customers.csv','r')
+#outfile = open('customer_country.csv' , 'w')
+
+#csvfile = csv.reader(infile, delimiter=',')
+
+#next(csvfile)
+#outfile.write('Full Name, Country\n')
+#for record in csvfile:
+    #full_name = record[1] + " " + record[2]
+    #country = record[4]
+
+    #outfile,write(full_name + ',' + country)
+
+    #outfile.close()
+
